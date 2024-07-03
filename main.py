4fb6727f80a574be3101018c9fb2967aec8c5d68
roho2 <rjhollinger@gmail.com>
2024-07-02 20:42:34 -0400

    Added input functionality and basic verification of url

--- main.py
+++ main.py
@@ -1,6 +1,42 @@
+import sys
+
+
 def main():
+    url = getYoutubeURL()
+    unparsed_result = makeAPICall(url)
+
+
+def getYoutubeURL() -> str:
+    printHeader()
+    url = input("Please enter a URL to a youtube video: ")
+    if checkURLValidity(url):
+        return url
+    else:
+        sys.exit("Invalid URL!!!")
+
+
+def checkURLValidity(url) -> bool:
+    if url.isnumeric:
+        return False
+    elif url.strip() == "":
+        return False
+    else:
+        return True
+
+
+def printHeader():
+    print("----------------------------------------------------------------------")
+    print("-            Welcome to Python Youtube Statistics Fetcher            -")
+    print("-                            Version 0.1                             -")
+    print("-                    Created by Robert Hollinger                     -")
+    print("----------------------------------------------------------------------")
+
+
+def makeAPICall(url) -> str:
     ...
 
 
 if __name__ == "__main__":
     main()
+
+# Black, mypy, pycheck?, pytest of course
