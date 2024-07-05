a7ffd5f02d78c28f4251e8d4d22dc00c1890b386
roho2 <rjhollinger@gmail.com>
2024-07-05 10:52:13 -0400

    Updated to Version 1.0, first working version

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-# Wrapping up for the night. Got the basic get call working
-# Need to fix url checker, get it to take a full URL and filter out just the ID.
-# Then need to clean the json response, fill variables. Probably use a dictionary for this.
-
 import sys
 import requests
 import pprint
@@ -9,13 +5,12 @@ import pprint
 api_key = 'AIzaSyDEgOtAKuGUT   7IZpQKxDUTfyOsM_xp5bqk'
 
 
-# Video ID for lofi video: 'jfKfPfyJRdk'
-
 def main():
     url = getYoutubeURL()
     video_id = getVideoId(url)
     title_response, statistics_response = makeAPICall(video_id)
     info = getInfoFromResponses(title_response, statistics_response)
+    printVideoInformation(info)
 
 
 def getYoutubeURL() -> str:
@@ -27,7 +22,7 @@ def getYoutubeURL() -> str:
         sys.exit("Invalid URL!!!")
 
 
-def checkURLValidity(url) -> bool:
+def checkURLValidity(url: str) -> bool:
     if url.isnumeric():
         return False
     elif url.strip() == "":
@@ -39,17 +34,17 @@ def checkURLValidity(url) -> bool:
 def printHeader() -> None:
     print("----------------------------------------------------------------------")
     print("-            Welcome to Python Youtube Statistics Fetcher            -")
-    print("-                            Version 0.1                             -")
+    print("-                            Version 1.0                             -")
     print("-                    Created by Robert Hollinger                     -")
     print("----------------------------------------------------------------------")
 
 
-def getVideoId(url) -> str:
+def getVideoId(url: str) -> str:
     youtube, videoId = url.split('?v=')
     return videoId
 
 
-def getInfoFromResponses(title_response, statistics_response) -> dict:
+def getInfoFromResponses(title_response: dict, statistics_response: dict) -> dict:
     # We want to print Title, views, likes, dislikes (if possible), comment count, and favorite count.
     info = {'title': title_response['items'][0]['snippet']['title'],
             'views': statistics_response['items'][0]['statistics']['viewCount'],
@@ -59,17 +54,24 @@ def getInfoFromResponses(title_response, statistics_response) -> dict:
     return info
 
 
-def makeAPICall(video_id) -> tuple[dict, dict]:
-    # Response1 has 'title', 'publishedAt' (in format 2024-06-30T19:00:26Z)
+def makeAPICall(video_id: str) -> tuple[dict, dict]:
+    # 'title', 'publishedAt' (in format 2024-06-30T19:00:26Z)
     titleResponse = requests.get(f'https://www.googleapis.com/youtube/v3/videos?part=snippet&id={video_id}&key={api_key}')
-    # Response2 has a dict with key 'items[]'->'statistics{}'->'commentCount', 'favoriteCount', 'likeCount', 'viewCount'
-    statistics = requests.get(f'https://www.googleapis.com/youtube/v3/videos?part=statistics&id={video_id}&key={api_key}')
-    return titleResponse.json(), statistics.json()
+    # dict with key 'items[]'->'statistics{}'->'commentCount', 'favoriteCount', 'likeCount', 'viewCount'
+    statisticsResponse = requests.get(f'https://www.googleapis.com/youtube/v3/videos?part=statistics&id={video_id}&key={api_key}')
+    return titleResponse.json(), statisticsResponse.json()
+
+
+def printVideoInformation(info):
+    print("===========================================================================")
+    print(f"Video Title: {info['title']}")
+    print(f"Views: {info['views']}")
+    print(f"Likes: {info['likes']}")
+    print(f"Comments: {info['comments']}")
+    print(f"Favorites: {info['favorites']}")
 
 
 if __name__ == "__main__":
     main()
-    # makeAPICall('jfKfPfyJRdk')
 
 # Black, mypy, pycheck?, pytest of course
-# https://stackoverflow.com/questions/26199933/youtube-api-3-0-search-videos-and-get-video-statistics-at-single-request
